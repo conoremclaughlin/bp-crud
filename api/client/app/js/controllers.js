bde6f2d3683cb0cb456ce1733acdf67daf178f6d
Conor McLaughlin <conoremclaughlin@gmail.com>
2013-03-08 00:05:06 -0800

    cleaning up and sticking to the fundamentals.

--- api/client/app/js/controllers.js
+++ api/client/app/js/controllers.js
@@ -4,89 +4,25 @@
 
 angular.module('app.controllers', [])
 
-    .controller('BillList', ['$scope', 'Transaction', function($scope, Transaction) {
-        console.log('Bill List has been called.');
-        Transaction.query(function(bills) {
-            console.log('bills: ', bills);
-            $scope.bills = bills.data;
-        }, function(error) {
-            console.error('error retreiving users: ', error);
-        });
-
-        $scope.orderProp = 'age';
-    }])
-
-    .controller('BillDetails', ['$scope', '$routeParams', 'Transaction', function($scope, $routeParams, Transaction) {
-        $scope.submit = function submit() {
-            var transDocument = new Transaction(this.bill);
-            transDocument.$save(function() {
-
-            }, function(error) {
-                alert('Error creating your bill.  Please try again later.');
-            });
-        };
-    }])
-
-    .controller('UserList', ['$scope', 'User', function($scope, User) {
-        User.query(function(users) {
+    .controller('UserList', ['$scope', 'UserResource', function($scope, User) {
+        User.Resource.query(function(users) {
             $scope.users = users.data;
         }, function(error) {
             console.error('error retreiving users: ', error);
         });
-
-        $scope.orderProp = 'age';
     }])
 
-    .controller('UserDetails', ['$scope', '$routeParams', '$location', 'User', function($scope, $routeParams, $location, User) {
+    .controller('UserDetails', ['$scope', '$routeParams', '$location', 'UserResource', function($scope, $routeParams, $location, User) {
         // populate the proper user
-        User.get({ userId: $routeParams.userId }, function(user) {
-            console.log('user: ', user);
-            console.log('$scope.user: ', $scope.user);
-            $scope.user = user;
+        User.Resource.get({ id: $routeParams.id }, function(user) {
+            $scope.user = new User({ model: user });
+            $scope.id = $routeParams.id;
         }, function(error) {
-            console.error('error retreiving users: ', error);
+            console.error('error retreiving user: ', error);
         });
-
-        $scope.submit = function submit() {
-            var userDocument = new User(this.user);
-            console.log('userDocument: ', userDocument);
-            // TODO: remove new User - may not be needed.
-            console.log('userDocument: ', this.user);
-
-            userDocument.$update({ userId: $routeParams.userId }, function() {
-                // TODO: alert it's been updated.
-                $location.path('/users');
-            }, function(error) {
-                alert('We cannot update your profile at this time.  Please try again later.');
-            });
-        };
-
-        $scope.del = function del() {
-            User.delete({ userId: $routeParams.userId }, function() {
-                console.log('deleted!');
-                $location.path('/users');
-            }, function() {
-                console.error('not deleted!');
-            });
-        };
     }])
 
-    .controller('UserNew', ['$scope', '$routeParams', '$location', 'User', function($scope, $routeParams, $location, User) {
-        $scope.user = {
-            username: '',
-            email: '',
-            firstName: '',
-            lastName: ''
-        };
-
-        $scope.submit = function submit() {
-            var userDocument = new User(this.user);
-            userDocument.$save(function(user) {
-                // TODO: alert it's been saved.
-                $location.path('/users');
-            }, function(error) {
-                alert('We cannot create your profile at this time.  Our apologies.  Please try again later.');
-            });
-        };
+    .controller('UserNew', ['$scope', '$routeParams', '$location', 'UserResource', function($scope, $routeParams, $location, User) {
+        $scope.user = new User({});
     }])
 ;
--- api/client/app/js/services.js
+++ api/client/app/js/services.js
@@ -4,17 +4,108 @@
 
 angular.module('app.services', ['ngResource'])
 
-    .factory('User', function($resource) {
-        return $resource('api/users/:userId/', {}, {
-            query: { method: 'GET', isArray: false },
-            update: { method: 'PUT' }
-        });
-    })
+    .factory('ModelResource', ['$resource', '$location', '$http', function($resource, $location, $http) {
+        // TODO: abstract to .value() and make more flexible? hmm...
+        var urlRoot = '';
 
-    .factory('Transactions', function($resource) {
-        return $resource('api/transactions/:transactionId/', {}, {
-            query: { method: 'GET', isArray: false },
-            update: { method: 'PUT' }
+        function Model() {
+            this.initialize.apply(this, arguments);
+        }
+
+        Model.extendResource = function(url, Class) {
+            var idUrl = urlRoot + '/api' + url + '/:id/';
+            Class.prototype = Object.create(Model.prototype);
+            Class.prototype.url = url;
+            Class.prototype.apiUrl = function(id) {
+                var url = urlRoot + '/api' + this.url + '/';
+                url += (id || this.id) ? (id || this.id) + '/' : '';
+                return url;
+            };
+
+            // array fix for mongodb records returned form the API
+            Class.prototype.Resource = $resource(idUrl, {}, {
+                query: { method: 'GET', isArray: false }
+            });
+
+            Class.Resource = Class.prototype.Resource;
+
+            return Class;
+        };
+
+        Model.prototype.schema = {};
+
+        Model.prototype.initialize = function initialize(options, bootstrap) {
+            options = options || {};
+
+            // cloning the schema into the model - @see http://jsperf.com/cloning-an-object/2
+            // this behavior may be unwanted depending on space complexity and use cases
+            this.model = JSON.parse(JSON.stringify(this.schema));
+            this.url = this.url || options.url || '/';
+            this.path = options.path || '';
+
+            if (options.model) {
+                // note: this filter does not do a deep recursion checking of the schema.
+                for (var prop in options.model) {
+                    if (this.schema.hasOwnProperty(prop)) this.model[prop] = options.model[prop];
+                }
+            }
+
+            return this;
+        };
+
+        Model.prototype.del = function del(id) {
+            var that = this;
+            id = id || this.model.id;
+
+            this.Resource.remove({ id: id }, function() {
+                $location.path(that.url);
+            }, function() {
+                alert('We cannot reach the API at this time.  Our apologies.  Please try again later.');
+            });
+        };
+
+        Model.prototype.create = function create(model) {
+            var that = this;
+            model = model || this.model;
+
+            // TODO: check if API allows calling this.Resource directly
+            $http.post(urlRoot + '/api' + this.url + '/', model)
+                .success(function() {
+                    $location.path(that.url);
+                })
+                .error(function() {
+                    alert('We cannot reach the API at this time.  Our apologies.  Please try again later.');
+                });
+        };
+
+        Model.prototype.update = function update(id) {
+            var that = this;
+            id = id || this.model.id;
+
+            $http.put(this.apiUrl(id), this.model, function() {
+                // TODO: alert it's been updated.
+                $location.path(that.url);
+            }, function(error) {
+                alert('We cannot update your profile at this time.  Please try again later.');
+            });
+        };
+
+        return Model;
+    }])
+
+    .factory('UserResource', ['ModelResource', function(Model) {
+
+        var User = Model.extendResource('/users', function User() {
+            Model.apply(this, arguments);
         });
-    })
+
+        User.prototype.schema = {
+            username: '',
+            email: '',
+            firstName: '',
+            lastName: ''
+        };
+
+        return User;
+    }])
 ;
